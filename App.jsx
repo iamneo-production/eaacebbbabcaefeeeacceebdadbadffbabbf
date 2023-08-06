8e2a42b73116608de58a8a879b6f96bdf31a933a
Iamneo <techadmin@iamneo.ai>
2023-08-06 18:28:25 +0000

    Backup Commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+
+import logo from "./logo.svg";
+import "./App.css";
+import Stopwatch from "./components/Stopwatch/Stopwatch";
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
